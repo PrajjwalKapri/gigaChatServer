0e9830121f0300cd0192594fead410e18250461b
prajjwalkapri <prajjwalkapri@gmail.com>
2024-06-15 23:21:50 +0530

    backend bug fixes

--- constants/config.mjs
+++ constants/config.mjs
@@ -1,6 +1,10 @@
-corsOptions = {
-  origin: process.env.CLIENT_URL,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
